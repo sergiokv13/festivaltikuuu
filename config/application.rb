9c41ec9daf93afcddab925abf0d6303b2252d2e9
Alexander Peredo Torossova <aptpta@yahoo.es>
2016-02-13 14:19:12 -0400

    Arreglos

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module FestivalTinku
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.assets.precompile += [
+      'bootstrap.min.css',
+      'font-awesome.min.css',
+      'main.css',
+      'responsive.css'
+    ]
   end
 end
